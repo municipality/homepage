cdccd596874055c0fdd75c196ddcdde29f685dba
Brian <amunicipality@gmail.com>
2016-05-20 09:36:01 -0700

    allow cors on server

--- app.js
+++ app.js
@@ -31,12 +31,11 @@ app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.all('/', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
- });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 var router = express.Router();
 
