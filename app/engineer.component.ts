f723a8c0884876b21c7d60d6e250440f454ff8fa
Brian <amunicipality@gmail.com>
2016-06-03 10:22:09 -0700

    blank

--- app/engineer.component.ts
+++ app/engineer.component.ts
@@ -1,1 +1,10 @@
 import {Component} from 'angular2/core';
+
+@Component ({
+    selector : 'engineer',
+    template : `
+        <div class="engineer-container">
+            
+        </div>
+    `
+})
