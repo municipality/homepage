37eda506d3040772983226587c2c06b4fd9f952a
Brian <amunicipality@gmail.com>
2016-06-18 00:25:30 +0800

    footer more size friendly realign objectives

--- app/footer.component.ts
+++ app/footer.component.ts
@@ -3,25 +3,24 @@ import {Component} from 'angular2/core';
 @Component ({
     selector : 'footer',
     template : `
-        <div class="logo">
-            <img class="logo logo-image" src="images/logo.png">
-        </div>
         <div class="container">
-            <div class="cell">
-                <p>Software Engineer</p>
-                <p>Designer</p>
+            <div class="logo cell">
+                <img class="logo logo-image" src="images/logo.png">
             </div>
-            <div class="cell">
-                <h4>Contact</h4>
-                <a class="email link" href="mailto:ucsdblee@gmail.com">ucsdblee@gmail.com</a>
-            </div>
-            <div class="cell">
-                <h4>Follow My Ventures</h4>
-                <a class="ig-name link" href="http://instagram.com/best_blee_it" target="_blank"><img class="ventures-icon" src='images/icons/instagram-icon.png'>@best_blee_it</a>
-                <a class="linkedin-name link" href="http://linkedin.com/in/bleee" target="_blank"><img class="ventures-icon" src="images/icons/linkedin-icon.png">@bleee</a>
-            </div>
+                <div class="cell">
+                    <p>Software Engineer</p>
+                    <p>Designer</p>
+                </div>
+                <div class="cell">
+                    <h4>Contact</h4>
+                    <a class="email link" href="mailto:ucsdblee@gmail.com">ucsdblee@gmail.com</a>
+                </div>
+                <div class="cell">
+                    <h4>Follow My Ventures</h4>
+                    <a class="ig-name link" href="http://instagram.com/best_blee_it" target="_blank"><img class="ventures-icon" src='images/icons/instagram-icon.png'>@best_blee_it</a>
+                    <a class="linkedin-name link" href="http://linkedin.com/in/bleee" target="_blank"><img class="ventures-icon" src="images/icons/linkedin-icon.png">@bleee</a>
+                </div>
         </div>
-
         <div class="last-line">
             <p>&copy;Brian Lee</p>
             <p>Built with Angular2</p>
